ee313428ca9b121e329f9e6acfcfe83a8ee12ef8
Almuheetu Shihab <almuheetu@gmail.com>
2024-02-16 01:07:09 +0600

    fixed

--- script/triangle-calculate.js
+++ script/triangle-calculate.js
@@ -1,4 +1,13 @@
 function calculateTriangleArea(){
     const triangleBaseInput = document.getElementById('triangle-base');
-    console.log(triangleBaseInput);
+    const triangleBaseText = triangleBaseInput.value;
+    const base = parseFloat(triangleBaseText);
+    // console.log(base);
 }
+
+
+
+
+const triangleHeightInput = document.getElementById('triangle-height');
+const triangleHeightInput = 
+console.log(triangleHeightInput);
